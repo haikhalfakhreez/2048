cd0eb6ca9451738a156028e6cf6801f9c8fa248d
Haikhal Fakhreez <haikhalfakhreez@gmail.com>
2020-11-04 09:06:34 +0800

    swipe

--- app.js
+++ app.js
@@ -275,58 +275,68 @@ document.addEventListener('DOMContentLoaded', () => {
     }
 
     // swipe
-    
-    // usage example
-    detectSwipe('swipeme', (el, dir) => alert(`you swiped on element with id ${el.id} to ${dir} direction`))
-
-    // source code
-
-    // Tune deltaMin according to your needs. Near 0 it will almost
-    // always trigger, with a big value it can never trigger.
-    function detectSwipe(id, func, deltaMin = 90) {
-    const swipe_det = {
-        sX: 0,
-        sY: 0,
-        eX: 0,
-        eY: 0
+    function detectswipe(el,func) {
+        swipe_det = new Object();
+        swipe_det.sX = 0; swipe_det.sY = 0; swipe_det.eX = 0; swipe_det.eY = 0;
+        var min_x = 30;  //min x swipe for horizontal swipe
+        var max_x = 30;  //max x difference for vertical swipe
+        var min_y = 50;  //min y swipe for vertical swipe
+        var max_y = 60;  //max y difference for horizontal swipe
+        var direc = "";
+        ele = document.getElementById(el);
+        ele.addEventListener('touchstart',function(e){
+          var t = e.touches[0];
+          swipe_det.sX = t.screenX; 
+          swipe_det.sY = t.screenY;
+        },false);
+        ele.addEventListener('touchmove',function(e){
+          e.preventDefault();
+          var t = e.touches[0];
+          swipe_det.eX = t.screenX; 
+          swipe_det.eY = t.screenY;    
+        },false);
+        ele.addEventListener('touchend',function(e){
+          //horizontal detection
+          if ((((swipe_det.eX - min_x > swipe_det.sX) || (swipe_det.eX + min_x < swipe_det.sX)) && ((swipe_det.eY < swipe_det.sY + max_y) && (swipe_det.sY > swipe_det.eY - max_y) && (swipe_det.eX > 0)))) {
+            if(swipe_det.eX > swipe_det.sX) direc = "r";
+            else direc = "l";
+          }
+          //vertical detection
+          else if ((((swipe_det.eY - min_y > swipe_det.sY) || (swipe_det.eY + min_y < swipe_det.sY)) && ((swipe_det.eX < swipe_det.sX + max_x) && (swipe_det.sX > swipe_det.eX - max_x) && (swipe_det.eY > 0)))) {
+            if(swipe_det.eY > swipe_det.sY) direc = "d";
+            else direc = "u";
+          }
+      
+          if (direc != "") {
+            if(typeof func == 'function') func(el,direc);
+          }
+          direc = "";
+          swipe_det.sX = 0; swipe_det.sY = 0; swipe_det.eX = 0; swipe_det.eY = 0;
+        },false);  
     }
-    // Directions enumeration
-    const directions = Object.freeze({
-        UP: 'up',
-        DOWN: 'down',
-        RIGHT: 'right',
-        LEFT: 'left'
-    })
-    let direction = null
-    const el = document.getElementById(id)
-    el.addEventListener('touchstart', function(e) {
-        const t = e.touches[0]
-        swipe_det.sX = t.screenX
-        swipe_det.sY = t.screenY
-    }, false)
-    el.addEventListener('touchmove', function(e) {
-        // Prevent default will stop user from scrolling, use with care
-        // e.preventDefault();
-        const t = e.touches[0]
-        swipe_det.eX = t.screenX
-        swipe_det.eY = t.screenY
-    }, false)
-    el.addEventListener('touchend', function(e) {
-        const deltaX = swipe_det.eX - swipe_det.sX
-        const deltaY = swipe_det.eY - swipe_det.sY
-        // Min swipe distance, you could use absolute value rather
-        // than square. It just felt better for personnal use
-        if (deltaX ** 2 + deltaY ** 2 < deltaMin ** 2) return
-        // horizontal
-        if (deltaY === 0 || Math.abs(deltaX / deltaY) > 1)
-        direction = deltaX > 0 ? directions.RIGHT : directions.LEFT
-        else // vertical
-        direction = deltaY > 0 ? directions.UP : directions.DOWN
-
-        if (direction && typeof func === 'function') func(el, direction)
-
-        direction = null
-    }, false)
+      
+    function myDown(el,d) {
+        moveDown();
+    }
+    function myUp(el,u) {
+        moveUp();
+    }
+    function myLeft(el,l) {
+        moveLeft();
+    }
+    function myRight(el,r) {
+        moveRight();
+    }
+    function control(e) {
+        if (myRight) {
+            keyRight();
+        }else if (myLeft) {
+            keyLeft();
+        }else if (myUp) {
+            keyUp();
+        }else if (myDown) {
+            keyDown();
+        }
     }
 
 })
